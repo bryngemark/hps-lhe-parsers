1a56c1b3bf4ecb9b88f52660313c93c230efe41f
Stanislava Sevova <ssevova@stanford.edu>
2020-01-17 07:19:06 -0800

    adding WAB LHE parser

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 g++ -g `root-config --cflags` lhetree_SIMP.cc -o lhetree_SIMP.exe `root-config --libs`
 #g++ -g -DDEBUG `root-config --cflags` lhetree_SIMP.cc -o lhetree_SIMP.exe `root-config --libs`
+g++ -g `root-config --cflags` lhetree_WAB.cc -o lhetree_WAB.exe `root-config --libs`
+#g++ -g -DDEBUG `root-config --cflags` lhetree_WAB.cc -o lhetree_WAB.exe `root-config --libs`
