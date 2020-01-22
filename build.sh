b39707fcf947630be2bb629004edc3ca055a674c
Stanislava Sevova <ssevova@stanford.edu>
2020-01-22 02:04:08 -0800

    add parser for tritrig and A' LHEs

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ g++ -g `root-config --cflags` lhetree_SIMP.cc -o lhetree_SIMP.exe `root-config -
 #g++ -g -DDEBUG `root-config --cflags` lhetree_SIMP.cc -o lhetree_SIMP.exe `root-config --libs`
 g++ -g `root-config --cflags` lhetree_WAB.cc -o lhetree_WAB.exe `root-config --libs`
 #g++ -g -DDEBUG `root-config --cflags` lhetree_WAB.cc -o lhetree_WAB.exe `root-config --libs`
+g++ -g `root-config --cflags` lhetree_TriTrig.cc -o lhetree_TriTrig.exe `root-config --libs`
+#g++ -g -DDEBUG `root-config --cflags` lhetree_TriTrig.cc -o lhetree_TriTrig.exe `root-config --libs`
+g++ -g `root-config --cflags` lhetree_Ap.cc -o lhetree_Ap.exe `root-config --libs`
+#g++ -g -DDEBUG `root-config --cflags` lhetree_Ap.cc -o lhetree_Ap.exe `root-config --libs`
